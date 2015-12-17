333b948e1f3fac21debb3cb1f4e245a102f81bf1
ykhandelwal <yogesh_khandelwal@gmail.com>
2015-12-17 21:07:35 +0530

    Adding files

---
+++ src/test/java/Pages/SamplePage.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package Pages;
+
+import java.lang.Thread;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.SwipeElementDirection;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.Dimension;
+
+/**
+ * A login page
+ */
+public class SamplePage extends BasePage {
+    /**
+     * The login button
+     */
+
+    private final double START_OFFSET = 0.9;
+    private final double END_OFFSET = 0.05;
+    private final int SWIPE_DURATION = 1000;
+
+    public TabViewPage tabViewPage;
+    @AndroidFindBy(id = "sliding_menu")
+    private MobileElement menuIcon;
+
+
+    @AndroidFindBy(id = "swipeText")
+    private MobileElement swipeText;
+
+    @AndroidFindBy(id = "sign_in")
+    private MobileElement signInButton;
+
+//
+//    @AndroidFindBy(id = "login_button")
+//    private MobileElement loginButton;
+
+    public SamplePage(AppiumDriver driver) {
+        super(driver);
+    }
+
+//    /**
+//     * The user name input
+//     */
+//    @AndroidFindBy(id = "username_text_input")
+//    private MobileElement userNameField;
+//
+//    /**
+//     * The password input
+//     */
+//    @AndroidFindBy(id = "password_text_input")
+//    private MobileElement passwordField;
+//
+//
+//
+//    /**
+//     * Tries to login with a set of credentials
+//     *
+//     * @param userName the username
+//     * @param password the password
+
+    public void swipe () {
+
+        Dimension size = driver.manage().window().getSize();
+        int startx = (int) (size.width * START_OFFSET);
+        int endx = (int) (size.width * END_OFFSET);
+        int starty = size.height / 2;
+
+        try {
+            Thread.sleep(10000);
+            driver.swipe(startx, starty, endx, starty, SWIPE_DURATION);
+            Dimension size1 = driver.manage().window().getSize();
+            int startx1 = (int) (size1.width * START_OFFSET);
+            int endx1 = (int) (size1.width * END_OFFSET);
+            int starty1 = size1.height / 2;
+            Thread.sleep(10000);
+            driver.swipe(startx1, starty1, endx1, starty1, SWIPE_DURATION);
+            Dimension size2 = driver.manage().window().getSize();
+            int startx2 = (int) (size2.width * START_OFFSET);
+            int endx2 = (int) (size2.width * END_OFFSET);
+            int starty2 = size2.height / 2;
+            Thread.sleep(10000);
+            driver.swipe(startx2, starty2, endx2, starty2, SWIPE_DURATION);
+            Dimension size3 = driver.manage().window().getSize();
+            int startx3 = (int) (size3.width * START_OFFSET);
+            int endx3 = (int) (size3.width * END_OFFSET);
+            int starty3 = size3.height / 2;
+            Thread.sleep(10000);
+            driver.swipe(startx3, starty3, endx3, starty3, SWIPE_DURATION);
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void clickMenu(){
+        menuIcon.click();
+    }
+
+    public void clickSignIn(){
+        signInButton.click();
+    }
+
+    public void loginIn(){
+        driver.findElementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.webkit.WebView[1]/android.webkit.WebView[1]/android.view.View[2]/android.view.View[5]/android.widget.EditText[1]").sendKeys("demouser8");
+        //android.view.View[1]/android.widget.FrameLayout[2]/android.webkit.WebView[1]/android.webkit.WebView[1]/android.view.View[2]/android.view.View[7]/android.widget.EditText[1]
+        driver.findElementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.webkit.WebView[1]/android.webkit.WebView[1]/android.view.View[2]/android.view.View[7]/android.widget.EditText[1]").sendKeys("demouser8");
+        driver.findElementByXPath("//android.view.View[1]/android.widget.FrameLayout[2]/android.webkit.WebView[1]/android.webkit.WebView[1]/android.view.View[2]/android.view.View[8]/android.view.View[1]/android.widget.Button[1]").click();
+//        userNameField.sendKeys(userName);
+//        passwordField.sendKeys(password);
+//        loginButton.click();
+    }
+
+//    /**
+//     *
+//     * @return the login message
+//     */
+//    public String getMessage(){
+//        return driver.findElementById("login_alt_message_textView").getText();
+//    }
+//
+//    /**
+//     * Checks to see if back at login page
+//     *
+//     * @return is back at login
+//     */
+//    public boolean checkIfBackAtLogin(){
+//        return loginButton.isDisplayed() && userNameField.isDisplayed() && passwordField.isDisplayed();
+//    }
+//
+//    /**
+//     * Presses the logout/try again button
+//     */
+//    public void pressAltButton(){
+//        driver.findElementById("alt_button").click();
+//    }
+}
---
+++ src/test/java/Tests/SampleTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package Tests;
+
+
+import Pages.SamplePage;
+import Tests.AbstractBaseTests.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * Tests for a login page
+ */
+public class SampleTest extends TestBase {
+
+
+    private SamplePage samplePage;
+
+    @Override
+    public String getName() {
+        return "Sample Page";
+    }
+
+    /**
+     * Creates a login
+     */
+    @BeforeTest
+    @Override
+    public void setUpPage() {
+        samplePage = new SamplePage(driver);
+    }
+
+    /**
+     * Tests logging in with valid credentials by verifying if the login message is correct
+     */
+
+//    @Test
+//    public void clickIniswipe(){
+//        samplePage.swipe();
+//    }
+
+
+    @Test
+    public void clickMenu(){
+         samplePage.clickMenu();
+    }
+
+    @Test
+    public void clickSignIn(){
+        samplePage.clickSignIn();
+    }
+
+    @Test
+    public void clickTsignIn(){
+        samplePage.loginIn();
+    }
+
+//    public void loginSuccessFully(){
+//        loginPage.loginIn(CORRECT_USER_NAME, CORRECT_PASSWORD);
+//        Assert.assertEquals(loginPage.getMessage(), LOGIN_SUCCESS_MESSAGE);
+//    }
+//
+//    /**
+//     * Tests logging in with invalid credentials by verifying if the error message is correct
+//     */
+//    @Test
+//    public void loginFail() {
+//        loginPage.loginIn(FAIL_USER_NAME, FAIL_PASSWORD);
+//        Assert.assertEquals(loginPage.getMessage(), LOGIN_FAIL_MESSAGE);
+//    }
+//
+//    /**
+//     * After each test method, logout or try again
+//     */
+//    @AfterMethod
+//    public void logOut(){
+//        loginPage.pressAltButton();
+//        Assert.assertTrue(loginPage.checkIfBackAtLogin());
+//    }
+}
